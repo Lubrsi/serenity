5b0f82faf5e1a5154142abed95141444465d0160
Luke <luke.wilde@live.co.uk>
2020-09-19 23:57:05 +0100

    fuzzjs updates

--- Meta/Lagom/Fuzzers/FuzzJs.cpp
+++ Meta/Lagom/Fuzzers/FuzzJs.cpp
@@ -24,25 +24,72 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <AK/String.h>
 #include <AK/StringView.h>
 #include <LibJS/Lexer.h>
 #include <LibJS/Parser.h>
+#include <LibJS/Runtime/GlobalObject.h>
+#include <signal.h>
+
 #include <stddef.h>
 #include <stdint.h>
 
+static const char* data;
+static size_t size;
+
+class TestRunnerGlobalObject : public JS::GlobalObject {
+public:
+    TestRunnerGlobalObject();
+    virtual ~TestRunnerGlobalObject() override;
+
+    virtual void initialize() override;
+
+private:
+    virtual const char* class_name() const override { return "TestRunnerGlobalObject"; }
+};
+
+TestRunnerGlobalObject::TestRunnerGlobalObject()
+{
+}
+
+TestRunnerGlobalObject::~TestRunnerGlobalObject()
+{
+}
+
+void TestRunnerGlobalObject::initialize()
+{
+    JS::GlobalObject::initialize();
+    define_property("global", this, JS::Attribute::Enumerable);
+}
+
+static void segfault_handler(int, siginfo_t*, void*) {
+    dbg() << "Segmentation fault!";
+    for (size_t i = 0; i < size; ++i) {
+        dbg() << String::format("%02x", data[size]) << " ";
+    }
+    ASSERT_NOT_REACHED();
+}
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
+    struct sigaction sa;
+
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = segfault_handler;
+
+    if (sigaction(SIGSEGV, &sa, NULL) == -1) {
+        ASSERT_NOT_REACHED();
+    }
+
     auto js = AK::StringView(static_cast<const unsigned char*>(data), size);
     auto lexer = JS::Lexer(js);
     auto parser = JS::Parser(lexer);
-    parser.parse_program();
-    if (parser.has_errors()) {
-        for (auto& error : parser.errors()) {
-            if (error.line >= 100000 || error.column >= 100000) {
-                fprintf(stderr, "%s\n", error.to_string().characters());
-                ASSERT_NOT_REACHED();
-            }
-        }
-    }
+    auto program = parser.parse_program();
+    if (parser.has_errors())
+        return 1;
+
+    auto interpreter = JS::Interpreter::create<TestRunnerGlobalObject>();
+    interpreter->run(interpreter->global_object(), *program);
     return 0;
 }
