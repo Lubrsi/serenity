04e5100b26d9ee8192c7bf85e15783b7289d2194
Luke Wilde <lukew@serenityos.org>
2021-10-10 07:01:43 +0100

    stuff 13

--- Userland/Libraries/LibWeb/DOM/EventTarget.cpp
+++ Userland/Libraries/LibWeb/DOM/EventTarget.cpp
@@ -30,6 +30,7 @@
 #include <LibJS/Runtime/AbstractOperations.h>
 #include <LibJS/Runtime/ObjectEnvironment.h>
 #include <LibWeb/HTML/HTMLFormElement.h>
+#include <LibWeb/Bindings/MainThreadVM.h>
 
 namespace Web::DOM {
 
@@ -358,8 +359,8 @@ void EventTarget::activate_event_handler(FlyString const& name, HTML::EventHandl
     //        For <body> or <frameset> elements who just had an element attribute set, it will be this's wrapper, as `this` is the result of determine_target_of_event_handler
     //        returning the element's document's global object, which is the DOM::Window object.
     //        For any other HTMLElement who just had an element attribute set, `this` will be that HTMLElement, so the global object is this's document's realm's global object.
-    //        For anything else, it came from JavaScript, so use the incumbent global object (the global object of the realm of the code that called this, but if this being done in a callback, it will be the global object of the code that caused the callback)
-    //        Sadly, this doesn't work if an element attribute is set on a body element before any script is run, as Window::wrapper() will be null.
+    //        For anything else, it came from JavaScript, so use the current global object from the main thread VM.
+    //        Sadly, this doesn't work if an element attribute is set on a <body> element before any script is run, as Window::wrapper() will be null.
     JS::GlobalObject* global_object = nullptr;
     if (is_attribute == IsAttribute::Yes) {
         if (is<Window>(this)) {
@@ -370,7 +371,7 @@ void EventTarget::activate_event_handler(FlyString const& name, HTML::EventHandl
             global_object = &html_element->document().realm().global_object();
         }
     } else {
-        global_object = &HTML::incumbent_global_object();
+        global_object = &Bindings::main_thread_vm().current_realm()->global_object();
     }
 
     VERIFY(global_object);
