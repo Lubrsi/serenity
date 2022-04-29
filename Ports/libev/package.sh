093e6e7d9cfd32bf1a1429d6fd8709f045c1d789
Luke Wilde <lukew@serenityos.org>
2022-04-29 22:27:34 +0100

    Ports: Add libev port

---
+++ Ports/libev/package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=libev
+version=4.33
+files="http://dist.schmorp.de/libev/libev-${version}.tar.gz libev-${version}.tar.gz 507eb7b8d1015fbec5b935f34ebed15bf346bed04a11ab82b8eee848c4205aea"
+auth_type=sha256
+useconfigure=true
---
+++ Ports/libev/patches/ReadMe.md
@@ -0,0 +1,10 @@
+# Patches for libev on SerenityOS
+
+## `add-serenity-to-config-sub.patch`
+
+config.sub: Add SerenityOS as portable system
+
+- [ ] Local?
+- [X] Should be merged to upstream?
+- [ ] Resolves issue(s) with our side of things
+- [ ] Hack
