7e8a182551d539052be67bd2a5ca82487304b47d
Luke Wilde <lukew@serenityos.org>
2022-04-29 20:10:06 +0100

    Ports: Add jansson port

---
+++ Ports/jansson/package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=jansson
+version=2.14
+files="https://github.com/akheron/jansson/releases/download/v${version}/jansson-${version}.tar.bz2 jansson-${version}.tar.bz2 fba956f27c6ae56ce6dfd52fbf9d20254aad42821f74fa52f83957625294afb9"
+auth_type=sha256
+useconfigure=true
