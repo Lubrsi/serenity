9fc2b6f672fb56dabd70102eaec0bce65c89b735
Luke Wilde <lukew@serenityos.org>
2022-04-29 20:08:21 +0100

    Ports/curl: Enable use of c-ares

--- Ports/curl/package.sh
+++ Ports/curl/package.sh
@@ -4,7 +4,7 @@ version=7.83.0
 useconfigure=true
 files="https://curl.se/download/curl-${version}.tar.bz2 curl-${version}.tar.bz2 247c7ec7521c4258e65634e529270d214fe32969971cccb72845e7aa46831f96"
 auth_type=sha256
-depends=("ca-certificates" "openssl" "zlib" "zstd")
+depends=("ca-certificates" "openssl" "zlib" "zstd" "c-ares")
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
 
 configure() {
@@ -18,7 +18,8 @@ configure() {
     -DCURL_DISABLE_NTLM=ON \
     -DCURL_DISABLE_SOCKETPAIR=ON \
     -DCURL_DISABLE_TESTS=ON \
-    -DCURL_HIDDEN_SYMBOLS=OFF
+    -DCURL_HIDDEN_SYMBOLS=OFF \
+    -DENABLE_ARES=ON
 }
 
 build() {
